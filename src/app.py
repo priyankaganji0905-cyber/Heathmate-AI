e20183f532f9baaaf310a77e27b8585014c83430
priyankaganji0905-cyber <priyankaganji0905@gmail.com>
2026-01-05 22:17:29 +0530

    Added patient Endpoint with sample data

--- src/app.py
+++ src/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "HealthMate is running!"
+
+@app.route("/patient")
+def patient():
+    patient_data = {
+        "name": "John Doe",
+        "age": 70,
+        "conditions": ["Hypertension", "Diabetes"]
+    }
+    return jsonify(patient_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
